604bebc31e9c23ed674a1579e477d8afe6c0ff13
Kimberly Ames <Kimberly Ames>
2016-11-14 13:18:17 -0700

    changes to PetsController

--- public/js/PetsController.ts
+++ public/js/PetsController.ts
@@ -1,14 +1,55 @@
 namespace App {
 
     export class PetsController {
-        //static $inject = ['$scope'];
-        static $inject = [];
+        static $inject = ['$http', '$state'];
+
+        private httpService;
+        private stateService;
+
+        public petsList;
+        public currentPet;
+        public newPet;
+
+        public name;
+        public category;
+        public breed;
+        public price;
 
         public title;
 
-        constructor (/*private $scope*/) {
+        constructor (
+            $http: angular.IHttpService,
+            $state: angular.ui.IState
+        ) {
+            this.httpService = $http;
+            this.stateService = $state;
+
             this.title = 'Pets Page';
+
+
+            console.log ('PetsController stateService: ', this.stateService);
+
+            this.petsList = [];
+            this.newPet = {};
+
+            this.getPetList ();
         }
+
+        public getPetList () {
+            console.log ('getPetList fired');
+
+            this.httpService ({
+                url: '/pets',
+                method: 'GET'
+            })
+            .success ((response) => {
+                console.log ('Test data: ', response);
+                this.petsList = response;
+            })
+            .error ((response) => {
+            });
+        }
+
     }
 
 }
